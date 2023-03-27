2c6014f3ba980f08da2707b92b6e85a77c3183a6
Krzysztof Baranowski <pharcosyle@gmail.com>
2023-03-27 09:16:00 -0700

    Update GCC and core packages

--- gnu/packages/groff.scm
+++ gnu/packages/groff.scm
@@ -34,6 +34,7 @@
   #:use-module (guix build-system ruby)
   #:use-module (guix gexp)
   #:use-module (gnu packages)
+  #:use-module (gnu packages base)
   #:use-module (gnu packages bash)
   #:use-module (gnu packages ruby)
   #:use-module (gnu packages bison)
@@ -63,7 +64,8 @@
    ;; own HTML doc, nor does it change its capabilities, so we removed netpbm
    ;; from 'inputs'.
 
-   (inputs (list ghostscript))
+   (inputs (list ghostscript
+                 diffutils-3.8)) ; `gdiffmk' is broken on newer diffutils.
 
    ;; When cross-compiling, this package depends upon a native install of
    ;; itself.
@@ -76,16 +78,20 @@
                     ("texinfo" ,texinfo)))
    (arguments
     `(#:parallel-build? #f   ; parallel build fails
-      ,@(if (%current-target-system)
-            `(#:make-flags
-              ;; In groff-minimal package, that inherits from this package,
-              ;; we'll need to locate "groff" instead of "self".
-              (let ((groff (or (assoc-ref %build-host-inputs "groff")
-                               (assoc-ref %build-host-inputs "self"))))
-                (list
-                 (string-append "GROFF_BIN_PATH=" groff)
-                 (string-append "GROFFBIN=" groff "/bin/groff"))))
-            '())
+      #:make-flags
+      (append
+       (list
+        (string-append "DIFF_PROG="
+                       (assoc-ref %build-inputs "diffutils") "/bin/diff"))
+       ,(if (%current-target-system)
+            ;; In groff-minimal package, that inherits from this package,
+            ;; we'll need to locate "groff" instead of "self".
+            `(let ((groff (or (assoc-ref %build-host-inputs "groff")
+                              (assoc-ref %build-host-inputs "self"))))
+               (list
+                (string-append "GROFF_BIN_PATH=" groff)
+                (string-append "GROFFBIN=" groff "/bin/groff")))
+            ''()))
       #:phases
       (modify-phases %standard-phases
         (add-after 'unpack 'disable-relocatability
@@ -137,7 +143,7 @@ is usually the formatter of \"man\" documentation pages.")
     (outputs '("out"))
 
     ;; Omit the DVI, PS, PDF, and HTML backends.
-    (inputs '())
+    (inputs (list diffutils-3.8)) ; `gdiffmk' is broken on newer diffutils.
     (native-inputs `(("bison" ,bison)
                      ("perl" ,perl)
                      ("groff" ,groff)))
