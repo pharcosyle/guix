dd271f655503de5e24c359cec397bc08f485926e
Krzysztof Baranowski <pharcosyle@gmail.com>
2023-04-19 00:20:25 -0700

    omnibump phase 5

--- gnu/packages/llvm.scm
+++ gnu/packages/llvm.scm
@@ -1496,11 +1496,11 @@ Library.")
   (make-clang-toolchain clang-17 libomp-17))
 
 ;; Default LLVM and Clang version.
-(define-public libomp libomp-13)
-(define-public llvm llvm-13)
-(define-public clang-runtime clang-runtime-13)
-(define-public clang clang-13)
-(define-public clang-toolchain clang-toolchain-13)
+(define-public libomp libomp-18)
+(define-public llvm llvm-18)
+(define-public clang-runtime clang-runtime-18)
+(define-public clang clang-18)
+(define-public clang-toolchain clang-toolchain-18)
 
 (define-public llvm-for-rocm
   (package
@@ -1653,7 +1653,7 @@ components which highly leverage existing libraries in the larger LLVM Project."
     (source (llvm-monorepo version))
     (inputs (list llvm-17))))
 
-(define-public lld lld-14)
+(define-public lld lld-18)
 
 (define* (make-lld-wrapper lld #:key lld-as-ld?)
   "Return a LLD wrapper.  When LLD-AS-LD? is true, create a 'ld' symlink that
