8bad6387a8e22967fc767764f85f733554f04a98
Krzysztof Baranowski <pharcosyle@gmail.com>
2023-04-18 21:09:37 -0700

    omnibump phase 3
    Change-Id: I2566d232e6d4bf94e419bbab3c3cc35d6616570d

--- gnu/packages/xml.scm
+++ gnu/packages/xml.scm
@@ -1855,16 +1855,24 @@ because lxml.etree already has its own implementation of XPath 1.0.")
 (define-public python-lxml
   (package
     (name "python-lxml")
-    (version "4.9.1")
+    (version "4.9.4")
     (source
      (origin
        (method url-fetch)
        (uri (pypi-uri "lxml" version))
        (sha256
-         (base32 "0grczyrrq2rbwhvpri15cyhv330s494vbz3js3jky8xp5c2rnx7y"))))
+         (base32 "03l86qr5xzvz0jcbk669sj8nbw1fjshmf0b7l83gl5cfnx81wm5i"))))
     (build-system python-build-system)
     (arguments
      `(#:phases (modify-phases %standard-phases
+                  ;; Should be fixed in 5.0, remove this then.
+                  ;; See https://bugs.launchpad.net/lxml/+bug/2016939
+                  (add-after 'unpack 'ignore-failing-test
+                    (lambda _
+                      (substitute* "src/lxml/tests/test_etree.py"
+                        ((".*def test_html_prefix_nsmap.*" all)
+                         (string-append "    @unittest.skipIf(True, "
+                                        "\"currently failing\")\n" all)))))
                   (replace 'check
                     (lambda* (#:key tests? #:allow-other-keys)
                       (when tests?
