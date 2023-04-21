53efd81f932f95c49d864baf6f8761c79a98a2eb
Krzysztof Baranowski <pharcosyle@gmail.com>
2023-04-21 09:42:41 -0700

    squash with networking
    Change-Id: Ib33c10b18843318ee07c0818c85762f20b8b177b

--- gnu/packages/admin.scm
+++ gnu/packages/admin.scm
@@ -947,7 +947,7 @@ re-executing them as necessary.")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnu/inetutils/inetutils-"
-                                  version ".tar.gz"))
+                                  version ".tar.xz"))
               (sha256
                (base32
                 "0q1257ci22g2jbdiqs00mharc1lqkbibdlkhj23f3si6qjxkn17s"))))
