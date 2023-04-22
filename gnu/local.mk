7db9eb489cbf0ae2965905ecf0ffc9b978f4257e
Krzysztof Baranowski <pharcosyle@gmail.com>
2023-04-22 15:52:09 -0700

    fontutils
    Change-Id: I6cd7f3ab6687c6fd01c2a71574b827454edd1a18

--- gnu/local.mk
+++ gnu/local.mk
@@ -1260,7 +1260,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/fluxbox-1.3.7-gcc.patch			\
   %D%/packages/patches/font-gnu-freefont-python3-compat.patch	\
   %D%/packages/patches/fontconfig-cache-ignore-mtime.patch	\
-  %D%/packages/patches/fontforge-hurd.patch			\
   %D%/packages/patches/foobillard++-pkg-config.patch		\
   %D%/packages/patches/foomatic-filters-CVE-2015-8327.patch	\
   %D%/packages/patches/foomatic-filters-CVE-2015-8560.patch	\
