495dc383d7df6776c6c1cb1b752c1a277bb7fd08
Krzysztof Baranowski <pharcosyle@gmail.com>
2023-04-23 12:05:11 -0700

    ghostscript

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -777,7 +777,7 @@ and should be preferred to it whenever a package would otherwise depend on
            fontforge
            freetype
            gd
-           ghostscript
+           ghostscrpt-9.56
            gmp
            graphite2
            harfbuzz
