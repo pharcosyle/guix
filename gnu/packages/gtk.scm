2f216ff31e36544fea178243e6c9c9852268b2d7
Krzysztof Baranowski <pharcosyle@gmail.com>
2023-04-24 11:05:34 -0700

    gtks without rest

--- gnu/packages/gtk.scm
+++ gnu/packages/gtk.scm
@@ -1031,8 +1031,7 @@ application suites.")
            harfbuzz
            iso-codes
            json-glib-minimal
-           libxml2
-           rest))
+           libxml2))
     (native-inputs
      (list docbook-xml-4.3
            docbook-xsl
@@ -1256,7 +1255,6 @@ application suites.")
            libpng
            libtiff
            python
-           rest
            tracker))                    ;for filechooser search support
     (propagated-inputs
      ;; Following dependencies are referenced in .pc files.
