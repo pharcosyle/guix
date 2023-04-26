3b5491fdc527a3902d0d8d92e15faaac396ea582
Krzysztof Baranowski <pharcosyle@gmail.com>
2023-04-25 20:53:48 -0700

    promote default postgresql to 15 (databases)

--- gnu/packages/databases.scm
+++ gnu/packages/databases.scm
@@ -1377,7 +1377,7 @@ pictures, sounds, or video.")
        (append opensp docbook-sgml-4.2)
        (delete "docbook-xml")))))
 
-(define-public postgresql postgresql-14)
+(define-public postgresql postgresql-15)
 
 (define-public timescaledb
   (package
