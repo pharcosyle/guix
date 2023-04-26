406181e172077417d62d5b3eb1da2cadd7a61869
Krzysztof Baranowski <pharcosyle@gmail.com>
2023-04-26 14:55:32 -0700

    umockdev (check)

--- gnu/packages/check.scm
+++ gnu/packages/check.scm
@@ -3327,7 +3327,7 @@ pre-condition.")
 (define-public umockdev
   (package
     (name "umockdev")
-    (version "0.17.13")
+    (version "0.17.17")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://github.com/martinpitt/umockdev/"
@@ -3335,7 +3335,7 @@ pre-condition.")
                                   "umockdev-" version ".tar.xz"))
               (sha256
                (base32
-                "1kqkraag5v1jl5qfv0mb3ckm8yq2im21mng08sbs9dh9c9pbyvkc"))))
+                "0v6zgws4w4i67vivra2l51jqcjw2cydp76garw1j1csqwjqj3ri0"))))
     (build-system meson-build-system)
     (arguments
      (list #:phases
