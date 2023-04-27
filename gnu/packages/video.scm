e5c53608295f3628fad2af359790134b984d4500
Krzysztof Baranowski <pharcosyle@gmail.com>
2023-04-26 19:45:54 -0700

    use imagemagick-6 for transcode (merge with wimagemagick bump)

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -361,7 +361,7 @@ user has installed.")
      (list alsa-lib
            ffmpeg-4
            freetype
-           imagemagick
+           imagemagick-6
            lame
            liba52
            libdv
