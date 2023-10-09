3501d37b2845924dbcd550fcd91628029d935d82
Bruno Victal <mirai@makinata.eu>
2023-10-09 21:06:49 +0100

    gnu: Remove unneeded libxml2 from native-inputs. (xmlto)
    The native-search-paths from libxslt are reused within xmlto to provide
    XML_CATALOG_FILES and SGML_CATALOG_FILES so it's unnecessary to include
    libxml2 for the same effect.
    
    * gnu/packages/wm.scm (i3status, awesome)[native-inputs]: Remove libxml2.
    * gnu/packages/xml.scm (opensp)[native-inputs]: Ditto.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>

--- gnu/packages/wm.scm
+++ gnu/packages/wm.scm
@@ -338,7 +338,7 @@ or musca).
      (list asciidoc
            perl
            pkg-config
-           docbook-xsl libxml2          ; for XML_CATALOG_FILES
+           docbook-xsl
            xmlto))
     (home-page "https://i3wm.org/i3status/")
     (synopsis "Status bar for i3bar, dzen2, xmobar or similar programs")
@@ -1220,7 +1220,6 @@ for wlroots-based Wayland compositors.")
            doxygen
            gperf
            imagemagick
-           libxml2 ;for XML_CATALOG_FILES
            lua-ldoc
            pkg-config
            xmlto))
