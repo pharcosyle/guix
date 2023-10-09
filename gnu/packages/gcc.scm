5acd18be26b1c90c7b2e7d711f27fcf36d52a460
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2023-10-09 11:10:48 -0400

    build-systems: gnu: Export %default-gnu-imported-modules and %default-gnu-modules.
    Until now users would have to cargo cult or inspect the private
    %default-modules variable of (guix build-systems gnu) to discover which
    modules to include when extending the used modules via the #:modules argument.
    
    The renaming was automated via the command:
    
      $ git grep -l %gnu-build-system-modules
        | xargs sed 's/%gnu-build-system-modules/%default-gnu-imported-modules/' -i
    
    * guix/build-system/gnu.scm (%gnu-build-system-modules): Rename to...
    (%default-gnu-imported-modules): ... this.
    (%default-modules): Rename to...
    (%default-gnu-modules): ... this.  Export.
    (dist-package, gnu-build, gnu-cross-build): Adjust accordingly.
    
    Change-Id: Idef307fff13cb76f3182d782b26e1cd3a5c757ee

--- gnu/packages/gcc.scm
+++ gnu/packages/gcc.scm
@@ -420,7 +420,7 @@ Go.  It also includes runtime support libraries for these languages.")
            ;; For native builds of some GCC versions the C++ include path needs to
            ;; be adjusted so it does not interfere with GCC's own build processes.
            (substitute-keyword-arguments (package-arguments parent)
-             ((#:modules modules %gnu-build-system-modules)
+             ((#:modules modules %default-gnu-imported-modules)
               `((srfi srfi-1)
                 ,@modules))
              ((#:phases phases)
@@ -932,7 +932,7 @@ using compilers other than GCC."
       #:out-of-source? #t
       #:modules `((srfi srfi-1)
                   (srfi srfi-26)
-                  ,@%gnu-build-system-modules)
+                  ,@%default-gnu-imported-modules)
       #:phases
       #~(modify-phases %standard-phases
           #$@(if (version>=? (package-version gcc) "11")
@@ -1080,7 +1080,7 @@ as the 'native-search-paths' field."
     (native-search-paths search-paths)
     (arguments
      (substitute-keyword-arguments (package-arguments gcc)
-       ((#:modules modules %gnu-build-system-modules)
+       ((#:modules modules %default-gnu-imported-modules)
         `(,@modules
           (srfi srfi-1)
           (srfi srfi-26)
