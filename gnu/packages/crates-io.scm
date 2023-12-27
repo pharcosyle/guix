1f33bc684e46b067a40d021ca2bb738037531b55
Efraim Flashner <efraim@flashner.co.il>
2023-12-27 09:55:42 +0200

    gnu: Remove rust-bindgen-0.57.
    * gnu/packages/crates-io.scm (rust-bindgen-0.57): Remove variable.
    (rust-bindgen-0.55, rust-bindgen-0.56): Inherit from rust-bindgen-0.58.
    
    Change-Id: I8e78221075ed334f0e224c2b100ee0cf71ba7e3a

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -7747,46 +7747,9 @@ bindings to C and C++ libraries.")
         ("rust-shlex" ,rust-shlex-1)
         ("rust-which" ,rust-which-3))))))
 
-(define-public rust-bindgen-0.57
-  (package
-    (inherit rust-bindgen-0.58)
-    (name "rust-bindgen")
-    (version "0.57.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "bindgen" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "0g96ypnck6n60p3cz9k3y3jnxi4z7jfv2pha5bxsm8269806aj7x"))))
-    (arguments
-     `(#:tests? #false ;tests/headers/16-byte-alignment.h does not exist
-       #:cargo-inputs
-       (("rust-bitflags" ,rust-bitflags-1)
-        ("rust-cexpr" ,rust-cexpr-0.4)
-        ("rust-clang-sys" ,rust-clang-sys-1)
-        ("rust-clap" ,rust-clap-2)
-        ("rust-env-logger" ,rust-env-logger-0.8)
-        ("rust-lazy-static" ,rust-lazy-static-1)
-        ("rust-lazycell" ,rust-lazycell-1)
-        ("rust-log" ,rust-log-0.4)
-        ("rust-peeking-take-while" ,rust-peeking-take-while-0.1)
-        ("rust-proc-macro2" ,rust-proc-macro2-1)
-        ("rust-quote" ,rust-quote-1)
-        ("rust-regex" ,rust-regex-1)
-        ("rust-rustc-hash" ,rust-rustc-hash-1)
-        ("rust-shlex" ,rust-shlex-0.1)
-        ("rust-which" ,rust-which-3))
-       #:cargo-development-inputs
-       (("rust-clap" ,rust-clap-2)
-        ("rust-diff" ,rust-diff-0.1)
-        ("rust-shlex" ,rust-shlex-0.1))))
-    (inputs
-     (list clang))))
-
 (define-public rust-bindgen-0.56
   (package
-    (inherit rust-bindgen-0.57)
+    (inherit rust-bindgen-0.58)
     (name "rust-bindgen")
     (version "0.56.0")
     (source (origin
@@ -7821,7 +7784,7 @@ bindings to C and C++ libraries.")
 
 (define-public rust-bindgen-0.55
   (package
-    (inherit rust-bindgen-0.57)
+    (inherit rust-bindgen-0.58)
     (name "rust-bindgen")
     (version "0.55.1")
     (source
