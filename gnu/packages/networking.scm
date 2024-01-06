348d9279e09b5cecfa39b55564e4c0bbf592ad79
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-01-06 03:40:51 -0800

    libcamera add python package (probably don't keep this commit once sphinx is back)
    Change-Id: Ic063bc2bb48a0b919b70b3f39fe24ffed2b1e5cf

--- gnu/packages/networking.scm
+++ gnu/packages/networking.scm
@@ -429,7 +429,8 @@ them in order to efficiently transfer a minimal amount of data.")
            pkg-config
            python-wrapper
            python-sphinx
-           python-pyyaml))
+           python-pyyaml
+           python-packaging)) ; Used in 'v4l_compat_test'.
     (inputs
      (list boost
            eudev
