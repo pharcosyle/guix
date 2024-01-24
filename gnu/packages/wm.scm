306b2c901f496a46622bd8a25d6f599e949c96ba
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-01-24 04:41:06 -0800

    wlroots and stuff WIP
    Change-Id: I0fed5f16490f84c1df7b1f8aa05c3dd337d6c460

--- gnu/packages/wm.scm
+++ gnu/packages/wm.scm
@@ -125,6 +125,7 @@
   #:use-module (gnu packages gperf)
   #:use-module (gnu packages gtk)
   #:use-module (gnu packages gnome)
+  #:use-module (gnu packages hardware)
   #:use-module (gnu packages haskell-check)
   #:use-module (gnu packages haskell-web)
   #:use-module (gnu packages haskell-xyz)
@@ -157,6 +158,7 @@
   #:use-module (gnu packages texinfo)
   #:use-module (gnu packages textutils)
   #:use-module (gnu packages time)
+  #:use-module (gnu packages vulkan)
   #:use-module (gnu packages web)
   #:use-module (gnu packages xdisorg)
   #:use-module (gnu packages xml)
@@ -1731,6 +1733,37 @@ Wayland compositor")
     (description "wlroots is a set of pluggable, composable, unopinionated
 modules for building a Wayland compositor.")
     (license license:expat)))  ; MIT license
+
+(define-public wlroots-0.17
+  (package
+    (inherit wlroots)
+    (version "0.17.1")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (inherit (origin-uri (package-source wlroots)))
+             (commit version)))
+       (file-name (git-file-name (package-name wlroots) version))
+       (sha256
+        (base32 "1hj4gq5vx8in65622yvjm8bwqkw2vpc556k9my997a0hn0ricj37"))))
+    (arguments
+     (substitute-keyword-arguments (package-arguments wlroots)
+       ((#:phases phases #~%standard-phases)
+        #~(modify-phases #$phases
+            (delete 'fix-meson-file)))))
+    (native-inputs
+     (modify-inputs (package-native-inputs wlroots)
+       (delete hwdata)))
+    (inputs
+     (modify-inputs (package-inputs wlroots)
+       (prepend hwdata
+                libdisplay-info
+                libliftoff
+                vulkan-loader ; TODO check it's used
+                )
+
+       ))))
 
 (define-public sway
   (package
