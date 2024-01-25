0191ffbfe6e3dee35021da9249cb3944742a7b5a
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-01-25 02:09:51 -0800

    Remove unnecesary python patch (squash with wherever I bumped python)
    Change-Id: Ieab5bfa9095f2d8093b339859789b64f7ce8002d

--- gnu/local.mk
+++ gnu/local.mk
@@ -1983,7 +1983,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/python-3-hurd-configure.patch		\
   %D%/packages/patches/python-angr-addition-type-error.patch	\
   %D%/packages/patches/python-angr-check-exec-deps.patch	\
-  %D%/packages/patches/python-3-reproducible-build.patch	\
   %D%/packages/patches/python-cffi-3.11-and-pytest-fixes.patch	\
   %D%/packages/patches/python-cross-compile.patch		\
   %D%/packages/patches/python-configobj-setuptools.patch	\
