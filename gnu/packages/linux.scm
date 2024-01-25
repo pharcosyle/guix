621f8c0ffb1e95cb72ed1b67a696e2e538de652f
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-01-25 09:34:17 -0800

    TON OF STUFF, SEPARATE ME
    Change-Id: I00a0d9be3b8b53a8d22b1f4d00d5f3f4c1676279

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -2213,7 +2213,7 @@ deviation, and minimum and maximum values.  It can show a nice histogram too.")
 (define-public util-linux
   (package
     (name "util-linux")
-    (version "2.39.3")
+    (version "2.40.2")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kernel.org/linux/utils/"
@@ -2221,7 +2221,7 @@ deviation, and minimum and maximum values.  It can show a nice histogram too.")
                                   "util-linux-" version ".tar.xz"))
               (sha256
                (base32
-                "03zdvxky2wnm1f0rc2x45x0da39x66gwbkxlqhyg8j8sipj0arkv"))
+                "1cz8r1kjnaiv40l6dzfaqg9lxlrkdcdb1prvvw7df8jrdyk3g2yp"))
               (patches (search-patches "util-linux-tests.patch"))
               (modules '((guix build utils)))
               (snippet
@@ -2252,7 +2252,11 @@ deviation, and minimum and maximum values.  It can show a nice histogram too.")
                    ;; Install completions where our bash-completion package
                    ;; expects them.
                    (string-append "--with-bashcompletiondir=" #$output
-                                  "/etc/bash_completion.d"))
+                                  "/etc/bash_completion.d")
+                   ;; Avoid a dependency on sqlite. There wouldn't be
+                   ;; anything wrong with that but I'd just as soon save
+                   ;; package size.
+                   "--disable-liblastlog2")
 
            ;; FIXME: For now we cannot reliably run tests on GNU/Hurd:
            ;; <https://bugs.gnu.org/47791>.
@@ -2282,7 +2286,19 @@ deviation, and minimum and maximum values.  It can show a nice histogram too.")
                      ;; Change the test to refer to the right file.
                      (substitute* "tests/ts/misc/mcookie"
                        (("/etc/services")
-                        services)))))
+                        services)))
+                   ;; /etc/fstab doesn't exist in the container, use another
+                   ;; file that does (chosen arbitrarily).
+                   (substitute* "tests/helpers/test_mkfds.c"
+                     (("/etc/fstab") "/etc/passwd"))
+                   ;; Not sure why this subtest fails, maybe something to do with
+                   ;; the behavior of exec/execve in the build container? Output:
+                   ;; "test_enosys: exec failed: No such file or directory"
+                   (substitute* "tests/ts/misc/enosys"
+                     (("ts_init_subtest exec" all)
+                      (string-append
+                       all "\n"
+                       "ts_skip \"skip failing enosys exec test\"")))))
                (add-before 'check 'disable-setarch-test
                  (lambda _
                    ;; The setarch tests are unreliable in QEMU's user-mode
@@ -9798,7 +9814,7 @@ types and interfaces and translates so that the X server can use them.")
 (define-public pipewire
   (package
     (name "pipewire")
-    (version "1.0.3")
+    (version "1.0.5")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -9807,7 +9823,7 @@ types and interfaces and translates so that the X server can use them.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "10psfk260pqgi375d5q80yyzy2a1jabs3cgvrd0w18sdwi1knp21"))))
+                "0fzmdkmi62r2jdw84vji9vfysc01a07qmd69fq507jyyrlvz02ln"))))
     (build-system meson-build-system)
     (arguments
      (list
