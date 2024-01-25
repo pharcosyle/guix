246c65ee92a3135db747b96f08a6e32125f3da38
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-01-25 07:28:37 -0800

    Emacs: add webp support
    Change-Id: I3108233b11e4db886f5cb3ac8ebb9215152b47e7

--- gnu/packages/emacs.scm
+++ gnu/packages/emacs.scm
@@ -478,6 +478,7 @@ editor (console only)")
                libpng
                (librsvg-for-system)
                libtiff
+               libwebp
                libx11
                libxft
                libxpm
