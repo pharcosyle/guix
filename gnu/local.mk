38fd2b0e6dedc6786480a3b8013fc4e31df22d3b
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-01-27 07:18:49 -0800

    fig2dev
    Change-Id: I2f557fde154a85f1d90d9951827d281cc9131869

--- gnu/local.mk
+++ gnu/local.mk
@@ -1201,6 +1201,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/fifengine-swig-compat.patch		\
   %D%/packages/patches/fifo-map-fix-flags-for-gcc.patch		\
   %D%/packages/patches/fifo-map-remove-catch.hpp.patch		\
+  %D%/packages/patches/fig2dev-ghostscript-10.02-fixes.patch	\
   %D%/packages/patches/file-32bit-time.patch			\
   %D%/packages/patches/findutils-localstatedir.patch		\
   %D%/packages/patches/firebird-riscv64-support-pt1.patch	\
