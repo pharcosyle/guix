8ffd597d58d31343a2c5381a2f9c5044a1683a78
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-02-12 22:54:26 -0800

    crypt interceptors patch for gcc-11 (prolly need to do it for more gcc's)
    Change-Id: I729555730568049563006c418bd7840f7c69f198

--- gnu/packages/gcc.scm
+++ gnu/packages/gcc.scm
@@ -731,7 +731,8 @@ It also includes runtime support libraries for these languages.")
              (base32
               "1ncd7akww0hl5kkmw1dj3qgqp3phdrr5dfnm7jia9s07n0ib4b9z"))
             (patches (search-patches "gcc-9-strmov-store-file-names.patch"
-                                     "gcc-5.0-libvtv-runpath.patch"))
+                                     "gcc-5.0-libvtv-runpath.patch"
+                                     "gcc-remove-crypt-interceptors.patch"))
             (modules '((guix build utils)))
             (snippet gcc-canadian-cross-objdump-snippet)))
    (arguments
