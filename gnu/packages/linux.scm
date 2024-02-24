0f132f3be8055790d47205e04f440182552878a3
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-02-24 06:15:13 -0800

    Use jack2-minimal
    Change-Id: I5a4f8997aa348e85ca653dd2e6d2251c4de4776d

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -9804,7 +9804,7 @@ types and interfaces and translates so that the X server can use them.")
                   ffmpeg
                   gst-plugins-base
                   gstreamer
-                  jack-2
+                  jack-minimal-2
                   ldacbt
                   libcamera
                   libfdk
