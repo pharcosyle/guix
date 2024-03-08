10c80c67f262139937ce41254c1d71c79fadcaae
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-03-07 17:18:08 -0800

    pipewire: reomve unused dependency on libva
    I'm pretty sure it's been defunct for a long time.
    
    Change-Id: Ib746a872a105585f541dfdb765044d9e4a9654e6

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -10125,7 +10125,6 @@ types and interfaces and translates so that the X server can use them.")
                   libsndfile
                   libusb
                   openssl ; raop sink
-                  libva
                   pulseaudio-minimal
                   readline ; for pw-cli
                   sbc
