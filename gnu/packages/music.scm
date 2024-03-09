0818d2b9c589e268921e918f3ba209f353dcfe52
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-03-09 13:20:25 -0800

    gtk-doc without dblatex
    Change-Id: Ic4aa81542eb7c45500b147c725a4ecefdd25c734

--- gnu/packages/music.scm
+++ gnu/packages/music.scm
@@ -1673,7 +1673,7 @@ and auto-mapping slices to MIDI note numbers.")
            python))
     (native-inputs
      (list bison
-           dblatex/stable
+           dblatex
            flex
            fontforge
            gettext-minimal
