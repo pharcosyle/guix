801b039f8a00887cdad24b8b3551d64e00de5b28
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-03-09 20:01:34 -0800

    improve appstream deps
    libsoup and lmdb were removed some time ago and shoud be
    removed. I also added vala because (as well as a configure flag to
    enable it which isn't the default) because we generally do that
    everywhere but I don't know about that part myself.
    
    Change-Id: Ic5dbf8330065ca684907a02475c6367be8138f07

--- gnu/packages/freedesktop.scm
+++ gnu/packages/freedesktop.scm
@@ -155,7 +155,8 @@
     (arguments
      (list
       #:glib-or-gtk? #t
-      #:configure-flags #~(list "-Dsystemd=false")
+      #:configure-flags #~(list "-Dsystemd=false"
+                                "-Dvapi=true")
       #:phases
       #~(modify-phases %standard-phases
           (add-after 'unpack 'patch-libstemmer
@@ -180,9 +181,10 @@
            itstool
            libxslt
            pkg-config
-           python-wrapper))
+           python-wrapper
+           vala))
     (inputs
-     (list curl libsoup-minimal-2 libstemmer libxmlb libxml2 libyaml lmdb))
+     (list curl libstemmer libxmlb libxml2 libyaml))
     (propagated-inputs
      (list glib))
     (synopsis "Tools and libraries to work with AppStream metadata")
