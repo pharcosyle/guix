ef9939627efc8e3ce3173305e5ebd1d42e4b9863
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-03-09 18:08:05 -0800

    small pipewire improment for dep's prop input
    Pipewire requires glib but it's not in the deps. Gstreamer propagates it
    hiding this. You nontice the problem if you're building without Gstreamer
    
    Change-Id: I5d0bd11809964bd211f0bbd8a6860cad12ff13fe

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -9883,6 +9883,7 @@ types and interfaces and translates so that the X server can use them.")
                   dbus
                   eudev
                   ffmpeg
+                  glib
                   gst-plugins-base
                   gstreamer
                   jack-minimal-2
