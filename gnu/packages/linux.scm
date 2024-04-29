6f466d07fa9f0f2ea04376aeeae69702e3570569
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-04-28 19:08:24 -0700

    bump pipewire again (squash with WIP)
    Change-Id: I11356a33f63a14014fa4c15ba91a345f6850f101

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -9388,7 +9388,7 @@ types and interfaces and translates so that the X server can use them.")
 (define-public pipewire
   (package
     (name "pipewire")
-    (version "1.0.3")
+    (version "1.0.5")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -9397,7 +9397,7 @@ types and interfaces and translates so that the X server can use them.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "10psfk260pqgi375d5q80yyzy2a1jabs3cgvrd0w18sdwi1knp21"))))
+                "0fzmdkmi62r2jdw84vji9vfysc01a07qmd69fq507jyyrlvz02ln"))))
     (build-system meson-build-system)
     (arguments
      (list
