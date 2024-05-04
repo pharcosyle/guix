53e6ee3f11894179986be80d1812380eb9a12ee9
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-05-04 23:49:12 +0800

    gnu: kde-cli-tools: Update to 6.0.4.
    * gnu/packages/kde-plasma.scm (kde-cli-tools): Update to 6.0.4.
    [arguments]: set #:qtbase to qtbase.
    <#:phases>: Remove symlink-kdesu phase.
    [inputs]: Add libxkbcommon. Remvoe qtx11extras. Use kconfig-6, kdesu-6,
    kdoctools-6, kiconthemes-6, ki18n-6, kcmutils-6, kio-6, kservice-6,
    kwindowsystem-6, plasma-activities, kparts-6 and qtsvg replace kconfig, kdesu,
    kdoctools, kiconthemes, ki18n, kcmutils, kio, kservice,
    kwindowsystem, kactivities, kparts and qtsvg-5.
    
    Change-Id: I86604adbf937a49d9a43095229577672ae016e7f

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -480,7 +480,7 @@ manager which re-parents a Client window to a window decoration frame.")
 (define-public kde-cli-tools
   (package
     (name "kde-cli-tools")
-    (version "5.27.7")
+    (version "6.0.4")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/plasma/"
@@ -488,10 +488,11 @@ manager which re-parents a Client window to a window decoration frame.")
               (patches (search-patches "kde-cli-tools-delay-mime-db.patch"))
               (sha256
                (base32
-                "1br1i8ba4n7d2yl618ph4glsaasn3rxy4kjp48f12l9l2pk29nxa"))))
+                "0vdfjpna14g2dhr5yck1wa737jz73dv6pm3qp2jd6x72w8zfkmir"))))
     (build-system qt-build-system)
     (arguments
-     (list #:tests? #f ;TODO: Failing 1 test
+     (list #:qtbase qtbase
+           #:tests? #f ;TODO: Failing 1 test
            #:phases
            #~(modify-phases %standard-phases
                (add-after 'unpack 'set-writable-location
@@ -502,28 +503,22 @@ GenericDataLocation.")
                       (string-append "\"" (getcwd) "/\"")))))
                (add-before 'check 'setup-env
                  (lambda* _
-                   (setenv "HOME" (getcwd))))
-               (add-after 'install 'symlink-kdesu
-                 (lambda _
-                   ;; XXX: nixpkgs say kdesu need kdeinit5 in PATH, but i can't
-                   ;; found in source, need check
-                   (symlink (string-append #$output "/libexec/kf5/kdesu")
-                            (string-append #$output "/bin/kdesu")))))))
+                   (setenv "HOME" (getcwd)))))))
     (native-inputs (list extra-cmake-modules pkg-config shared-mime-info))
-    (inputs (list kconfig
-                  kdesu
-                  kdoctools
-                  kiconthemes
-                  ki18n
-                  kcmutils
-                  kio
-                  kservice
-                  kwindowsystem
-                  kactivities
-                  kparts
+    (inputs (list kconfig-6
+                  kdesu-6
+                  kdoctools-6
+                  kiconthemes-6
+                  ki18n-6
+                  kcmutils-6
+                  kio-6
+                  kservice-6
+                  kwindowsystem-6
+                  plasma-activities
+                  kparts-6
                   plasma-workspace
-                  qtx11extras
-                  qtsvg-5))
+                  qtsvg
+                  libxkbcommon))
     (synopsis "CLI tools for interacting with KDE")
     (description "This package provides command-line tools based on
 KDE Frameworks 5 to better interact with the system.")
