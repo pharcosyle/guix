a169bb4fa508df4ecddc16acf5440ff4959f205f
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-05-15 14:22:25 -0700

    towards iw: remove wireless-tools from tlp
    Support for it was removed in tlp 1.5
    
    THIS IS UNTESTED (too many deps I don't wanna build) but seems like it
    should be fine
    
    Change-Id: I9465d9a2548abca01c85451912b03cdc73b45f36

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -8003,7 +8003,6 @@ interface in sysfs, which can be accomplished with the included udev rules.")
        ("sed" ,sed)
        ("usbutils" ,usbutils)
        ("util-linux" ,util-linux)
-       ("wireless-tools" ,wireless-tools)
        ,@(if (let ((system (or (%current-target-system)
                                (%current-system))))
                (or (string-prefix? "i686-" system)
@@ -8083,8 +8082,7 @@ interface in sysfs, which can be accomplished with the included udev rules.")
                                                "hdparm"
                                                "iw"
                                                "pciutils"
-                                               "rfkill"
-                                               "wireless-tools"))))))
+                                               "rfkill"))))))
                          bin-files)))))))
     (home-page "https://linrunner.de/en/tlp/tlp.html")
     (synopsis "Power management tool for Linux")
