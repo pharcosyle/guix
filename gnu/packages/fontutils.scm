bd5182a60f7da1c486e6c07ecb8f819cde3d6db0
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-05-24 00:06:20 -0700

    Towards no libungif: replace with giflib in fontforge

--- gnu/packages/fontutils.scm
+++ gnu/packages/fontutils.scm
@@ -1629,12 +1629,12 @@ definitions.")
     (inputs
      (list bash-minimal ; for wrap-program
            freetype
+           giflib
            glib
            libjpeg-turbo
            libpng
            libspiro
            libtiff
-           libungif
            libxml2
            potrace
            python
