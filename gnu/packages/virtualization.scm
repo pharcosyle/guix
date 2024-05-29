b7c2056ba6d95fd65b4f3f6d1b1aee22d645dcf9
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-05-29 05:56:06 -0700

    uncycling: fontconfig
    Change-Id: Ic825bb674703aa4c694d8b5ea6c9ca1fbe2e7bb1

--- gnu/packages/virtualization.scm
+++ gnu/packages/virtualization.scm
@@ -2194,7 +2194,7 @@ Machine Protocol.")
                                         "nettle"
                                         "mesa"
                                         "wayland"
-                                        "fontconfig-minimal"
+                                        "fontconfig"
                                         "freetype"
                                         "libx11"
                                         "libxfixes"
