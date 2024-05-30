4542b49f9ac11c94f07af77ab11eb08014e31745
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-05-29 18:15:04 -0700

    Create libinput-service-type
    Change-Id: I40cd5bbdead07b858a7ee91f326a07433fecd565

--- gnu/services/desktop.scm
+++ gnu/services/desktop.scm
@@ -1988,6 +1988,19 @@ applications needing access to be root.")
 
 
 ;;;
+;;; Libinput
+;;;
+(define-public libinput-service-type
+  (service-type
+   (name 'libinput)
+   (extensions
+    (list (service-extension udev-service-type list)))
+   (default-value libinput-minimal)
+   (description
+    "Install libinput udev rules.")))
+
+
+;;;
 ;;; The default set of desktop services.
 ;;;
 
@@ -2067,6 +2080,8 @@ applications needing access to be root.")
 
          (service pulseaudio-service-type)
          (service alsa-service-type)
+
+         (service libinput-service-type)
 
          %base-services))
 
