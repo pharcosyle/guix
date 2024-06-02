cdc718524f35948b9c9ade1325d2fef7b7d1a1f5
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-06-02 06:05:11 -0700

    TEMPORARY DELETE ME
    Change-Id: I064f86d51eae63658b19d4def44e5466afca726a

--- gnu/packages/gtk.scm
+++ gnu/packages/gtk.scm
@@ -2328,6 +2328,8 @@ with some extra work.")
      (inputs (modify-inputs (package-inputs gtk-doc-asdf)
                (delete "dblatex"))))))
 
+(define-public gtk-doc gtk-doc/stable)
+
 (define-public gtk-engines
   (package
     (name "gtk-engines")
