0dfca4f2331d0e315e1b6aedcfe8e7dd44abf756
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-06-02 14:01:13 -0700

    Make libdbusmenu gtk3-exclusive
    Change-Id: I2b6bce0a031ab1ae156c5b3cfd15833a21fb83ae

--- gnu/packages/gtk.scm
+++ gnu/packages/gtk.scm
@@ -2661,6 +2661,8 @@ printed to standard output.")
         #:configure-flags
         #~(list "--sysconfdir=/etc"
                 "--localstatedir=/var"
+                "--with-gtk=3"
+                "--disable-dumper"
                 ;; The shebang of the generated test files should be patched
                 ;; before enabling tests.
                 "--disable-tests")
@@ -2695,8 +2697,7 @@ printed to standard output.")
                 (setenv "HAVE_VALGRIND_FALSE" "#"))))))
       (inputs
        (list glib
-             gtk+
-             gtk+-2))
+             gtk+))
       (native-inputs
        (list autoconf
              automake
