pharcosyle/guix

commit 7a26aee61bbbd8e37745ce13433545333eac4505

gnu: pkgconf: Enable test suite.
* gnu/packages/pkg-config.scm (pkgconf) [arguments]: Remove #:tests? argument.
Add a set-HOME phase.
[native-inputs]: Add atf and kyua.

Change-Id: I28410d7486ab45cf1cbc6d306345fb85f0b9d7fc

gnu/packages/pkg-config.scm
@@ -2,7 +2,7 @@
 ;;; Copyright © 2012, 2013, 2014, 2016 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2019 Mathieu Othacehe <m.othacehe@gmail.com>
 ;;; Copyright © 2021 Maxime Devos <maximedevos@telenet.be>
-;;; Copyright © 2022 Maxim Cournoyer <maxim.cournoyer@gmail.com>
+;;; Copyright © 2022, 2024 Maxim Cournoyer <maxim.cournoyer@gmail.com>
 ;;; Copyright © 2023 Zheng Junjie <873216071@qq.com>
 ;;;
 ;;; This file is part of GNU Guix.
@@ -29,6 +29,7 @@
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system trivial)
   #:use-module (gnu packages bash)
+  #:use-module (gnu packages check)
   #:use-module (guix memoization)
   #:export (pkg-config))
 
@@ -181,7 +182,13 @@ exec ~a \"$@\""
                (base32
                 "0qbpczwrrsq2981mdv3iil26vq9ac8v1sfi9233jpiaixrhmhv96"))))
     (build-system gnu-build-system)
-    (arguments (list #:tests? #f))      ;TODO: package kyua
+    (arguments
+     (list #:phases #~(modify-phases %standard-phases
+                        (add-after 'unpack 'set-HOME
+                          (lambda _
+                            ;; Kyua requires a writable HOME.
+                            (setenv "HOME" "/tmp"))))))
+    (native-inputs (list atf kyua))
     (home-page "http://pkgconf.org/")
     (synopsis "Package compiler and linker metadata toolkit")
     (description "@command{pkgconf} is a program which helps to configure

commit 1b3e589b38fb7b4d25b2622b8d61898839f0eae2

gnu: Avoid cycle involving (gnu packages pkg-config).
Fixes a regression introduced in
7a26aee61bbbd8e37745ce13433545333eac4505 whereby (gnu packages
pkg-config) would be caught in a cycle, leading to miscompilation of
package modules, in turn leading to failure to compute the Guix
derivation with exceptions like:

  1. &package-input-error:
      package: #<package libgc@8.2.4 7fee96c4dc60>
      input: ("_" #<syntax-transformer pkg-config>)

* gnu/packages/pkg-config.scm: Autoload (gnu packages check).

Change-Id: I624e701a0c411b6f3d9ad5e10dca6ad7cf552be4

gnu/packages/pkg-config.scm
@@ -29,7 +29,13 @@
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system trivial)
   #:use-module (gnu packages bash)
-  #:use-module (gnu packages check)
+
+  ;; Note: Because this module defines the 'pkg-config' macro, it cannot be
+  ;; caught in a cycle with other package modules or the macro wouldn't be
+  ;; visible at the time those other modules are compiled.  To fulfill that
+  ;; constraint, load (gnu packages check) lazily.
+  #:autoload   (gnu packages check) (atf kyua)
+
   #:use-module (guix memoization)
   #:export (pkg-config))
 