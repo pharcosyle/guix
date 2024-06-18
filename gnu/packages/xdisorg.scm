a9725246135c8456353ed51e29e00f7bf3f20f36
宋文武 <iyzsong@member.fsf.org>
2024-06-18 11:53:18 +0800

    gnu: xscreensaver: Add libxcrypt dependency.
    * gnu/packages/xdisorg.scm (xscreensaver)[inputs]: Add libxcrypt.
    
    Change-Id: I0df314b4f83673c4262b1a493334b091ab89a9fb

--- gnu/packages/xdisorg.scm
+++ gnu/packages/xdisorg.scm
@@ -1800,6 +1800,7 @@ less if you are working in front of the screen at night.")
      (list pkg-config intltool))
     (inputs
      (list libx11
+           libxcrypt
            libxext
            libxi
            libxt
