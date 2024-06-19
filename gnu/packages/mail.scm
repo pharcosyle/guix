f46542f2a49291fda1034f16afc84d72b47b4318
宋文武 <iyzsong@member.fsf.org>
2024-06-19 11:17:09 +0800

    gnu: opensmtpd-extras: Fix build.
    * gnu/packages/mail.scm (opensmtpd-extras)[inputs]: Add libxcrypt.
    
    Change-Id: I7ea93d34cfb14434d5b47e52751d137dab4d7afe

--- gnu/packages/mail.scm
+++ gnu/packages/mail.scm
@@ -3350,6 +3350,7 @@ to esoteric or niche requirements.")
     (inputs
      `(("libressl" ,libressl)
        ("libevent" ,libevent)
+       ("libxcrypt" ,libxcrypt)         ;required by Python.h
        ("mysql" ,mariadb "dev")
        ("opensmtpd" ,opensmtpd)
        ("postgresql" ,postgresql)
