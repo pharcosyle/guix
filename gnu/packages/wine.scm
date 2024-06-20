0e7b63414e6cc88de7494ea847850cdd1da2f75d
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-06-20 12:34:54 -0700

    wine: add wine-devel and wine64-devel
    Change-Id: I62a6a912c23a078953263c4f7a57211e586c64c8

--- gnu/packages/wine.scm
+++ gnu/packages/wine.scm
@@ -314,6 +314,38 @@ integrate Windows applications into your desktop.")
     (synopsis "Implementation of the Windows API (WoW64 version)")
     (supported-systems '("x86_64-linux" "aarch64-linux"))))
 
+(define %wine-devel-version "9.17")
+
+(define-public wine-devel
+  (package
+    (inherit wine)
+    (name "wine-devel")
+    (version %wine-devel-version)
+    (source
+     (wine-source version
+                  "0308m8q3m3lwv2ppi0vwdm6pspyrm4yn5mcd6zvw7d8vyfv7xnry"))))
+
+(define-public wine64-devel
+  (package
+    (inherit wine64)
+    (name "wine64-devel")
+    (version %wine-devel-version)
+    (source
+     (wine-source version
+                  "0308m8q3m3lwv2ppi0vwdm6pspyrm4yn5mcd6zvw7d8vyfv7xnry"))
+    (inputs (modify-inputs (package-inputs wine64)
+              (replace "wine" wine-devel)))
+    (arguments
+     (substitute-keyword-arguments (package-arguments wine64)
+       ((#:phases phases)
+        #~(modify-phases #$phases
+            (replace 'copy-wine32-manpage
+              (lambda* (#:key inputs outputs #:allow-other-keys)
+                (let* ((out (assoc-ref %outputs "out")))
+                  ;; Copy the missing man file for the wine binary from wine.
+                  (copy-file (search-input-file inputs "/share/man/man1/wine.1.zst")
+                             (string-append out "/share/man/man1/wine.1.zst")))))))))))
+
 (define-public wine-staging-patchset-data
   (package
     (name "wine-staging-patchset-data")
