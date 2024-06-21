2ed1961b4c8c7b1d22789ac6d6c3f78600716440
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-06-21 11:53:50 -0700

    stash@{137} bump ed (squash with phase 1)
    Change-Id: I5c84f54edb6f65cbe35e970dcbe059c833cb3238

--- gnu/packages/text-editors.scm
+++ gnu/packages/text-editors.scm
@@ -113,14 +113,14 @@
 (define-public ed
   (package
     (name "ed")
-    (version "1.20")
+    (version "1.20.2")
     (source (origin
              (method url-fetch)
              (uri (string-append "mirror://gnu/ed/ed-"
                                  version ".tar.lz"))
              (sha256
               (base32
-               "19n5b4csjl1wwza1kwqa2flscxac0ma76vchhwb2y5xfwvphs0y6"))))
+               "08s5l9gpn1yy40ww4g5igc1n5zny0d3hzb1lycbwmhj8iwqwgzk5"))))
     (build-system gnu-build-system)
     (native-inputs (list lzip))
     (arguments
