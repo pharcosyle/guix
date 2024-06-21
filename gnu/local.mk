cd4d9f8597c4602ac8d4078c3c9dad44f3648087
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-06-21 12:00:50 -0700

    stash@{106} bump libxkbcommon (xdisorg)
    Change-Id: I297344073388657617c2c1f609da0994fd37dbe4

--- gnu/local.mk
+++ gnu/local.mk
@@ -1690,6 +1690,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/libvpx-CVE-2023-44488.patch		\
   %D%/packages/patches/libxml2-xpath0-Add-option-xpath0.patch	\
   %D%/packages/patches/libwpd-gcc-compat.patch			\
+  %D%/packages/patches/libxkbcommon-disable-x11comp-test.patch		\
   %D%/packages/patches/libxt-guix-search-paths.patch		\
   %D%/packages/patches/lierolibre-check-unaligned-access.patch	\
   %D%/packages/patches/lierolibre-is-free-software.patch	\
