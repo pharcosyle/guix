e5c2326be2a6a63e29ac35c8c0c0cca4b33fbb77
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-06-21 12:23:18 -0700

    stash@{4} system: wget -> wget2 in %base-packages-networking
    Other distros like Fedora have already switched over to having wget2 be
    their default
    
    Change-Id: I98cf7e6d08ee39f0c09a5fd3efe3b29c571b47dd

--- gnu/system.scm
+++ gnu/system.scm
@@ -973,7 +973,7 @@ of PROVENANCE-SERVICE-TYPE to its services."
   ;; Default set of networking packages.
   (list inetutils isc-dhcp
         iproute
-        wget
+        wget2
 	nss-certs
         iw))
 
