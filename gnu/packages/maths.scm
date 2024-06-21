47f4cd267b1cb3caffd2b7439eb58b714149e9b0
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-06-21 12:03:02 -0700

    rename autconf-0.71/0.69 usages
    Change-Id: I330af5469929a610f3b4c035b1921269331835e4

--- gnu/packages/maths.scm
+++ gnu/packages/maths.scm
@@ -9885,7 +9885,7 @@ computation is supported via MPI.")
                         "modules/ast/src/cpp/parse/parsescilab.cpp"))))))
     (build-system gnu-build-system)
     (native-inputs
-     (list autoconf-2.71
+     (list autoconf
            autoconf-archive
            automake
            bison
