0e62dc9a0df2fad5ce6c75fcb66e03c7ab885646
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-06-21 11:59:44 -0700

    stash@{113} towards pcre2: httpd
    Change-Id: I6c50983711b669c865053e63318c408172987eb8

--- gnu/packages/web.scm
+++ gnu/packages/web.scm
@@ -299,7 +299,7 @@
               (base32
                "1id45r2ccgkbjm9i998997ch32lvicpyynyx8x6aa4420wmdf5ps"))))
     (build-system gnu-build-system)
-    (native-inputs (list `(,pcre "bin")))       ;for 'pcre-config'
+    (native-inputs (list pcre2))       ;for 'pcre-config'
     (inputs (list apr apr-util libxcrypt openssl perl)) ; needed to run bin/apxs
     (arguments
      (list
