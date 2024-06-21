20bf6647dfdcbad5e25c41be473441c26ef005d1
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-06-21 12:13:05 -0700

    stash@{61} usb: usbutils: Bump to 017
    Change-Id: I00b360b5389027690e4e824c3896ad6ac13705ea

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -2568,6 +2568,10 @@ slabtop, tload, top, vmstat, w, watch and sysctl.")
     (outputs (list "out" "python"))
     (arguments
      (list
+      #:configure-flags
+      #~(list (string-append "--datadir="
+                             (assoc-ref %build-inputs "hwdata")
+                             "/share/hwdata"))
       #:phases
       #~(modify-phases %standard-phases
           (add-before 'bootstrap 'patch-bootstrap-scripts
@@ -2584,7 +2588,7 @@ slabtop, tload, top, vmstat, w, watch and sysctl.")
                             (rename-file old new)))
                         (list "bin/lsusb.py")))))))
     (inputs
-     (list eudev libusb python))
+     (list eudev hwdata libusb python))
     (native-inputs
      (list autoconf automake libtool pkg-config))
     (home-page "http://www.linux-usb.org/")
