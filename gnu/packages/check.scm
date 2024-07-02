21b5d0c0d4de6f683532ffa72a64529a0d2daf95
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-07-02 08:15:25 -0700

    python: tests: reduce python-nose usage
    Change-Id: Ib88148b4456684de051b6e7d6c87c0454c5824a8

--- gnu/packages/check.scm
+++ gnu/packages/check.scm
@@ -1632,7 +1632,6 @@ standard library.")
            ;; For tests.
            bash ; "Regular" bash since 'bash-final' lacks `compgen`.
            python-hypothesis
-           python-nose
            python-pytest-bootstrap
            python-xmlschema))
     (home-page "https://docs.pytest.org/en/latest/")
