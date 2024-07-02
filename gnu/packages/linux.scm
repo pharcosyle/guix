ae8a41decaf5a655a2b3b7de6ca1e3631bd62044
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-07-02 06:41:32 -0700

    quote input deletions?
    Was this working before and why isn't it now?
    
    Change-Id: I442d7ec2b4a2df450d7b11dac14382a858a4b372

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -9902,7 +9902,7 @@ tools for managing PipeWire.")
            #~(cons "-Delogind=disabled" #$configure-flags))))
       (inputs
        (modify-inputs (package-inputs base)
-         (delete elogind))))))
+         (delete "elogind"))))))
 
 (define-public ell
   (package
