368c9c00579de55262db5722d5421e400b942c03
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-07-02 01:13:21 -0700

    python: undo some way earlier stuff
    Change-Id: Ibb430e5394d31e6f19980b9057942f4b4a33ae08

--- gnu/packages/check.scm
+++ gnu/packages/check.scm
@@ -1404,7 +1404,8 @@ standard library.")
      (list python-exceptiongroup
            python-iniconfig
            python-packaging-bootstrap
-           python-pluggy))
+           python-pluggy
+           python-tomli))
     (native-inputs
      ;; Tests need the "regular" bash since 'bash-final' lacks `compgen`.
      (list bash
@@ -1445,7 +1446,8 @@ and many external plugins.")
   (package
     (inherit python-pytest)
     (name "python-pytest-bootstrap")
-    (native-inputs (list python-iniconfig python-setuptools-scm))
+    (native-inputs (list python-iniconfig python-setuptools-scm
+                         python-tomli))
     (arguments `(#:tests? #f))))
 
 (define-public python-pytest-assume
