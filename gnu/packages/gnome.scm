37e60de1d360bf03464df0ef2baa541547cfedc9
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-07-02 06:41:32 -0700

    quote input deletions?
    Was this working before and why isn't it now?
    
    Change-Id: I442d7ec2b4a2df450d7b11dac14382a858a4b372

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -8523,7 +8523,7 @@ services.")
                            #$configure-flags)))))
       (propagated-inputs
        (modify-inputs (package-propagated-inputs base)
-         (delete elogind))))))
+         (delete "elogind"))))))
 
 (define-public network-manager-openvpn
   (package
