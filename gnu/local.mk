b941daf2652cfee4b5ed9083b409edfd05391f91
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-07-05 01:23:18 -0700

    icu 75 fix for libical
    Also a bump, better to stay recent too.
    
    Change-Id: I983f56257f9f94921cd98c11514310c3a054d5d1

--- gnu/local.mk
+++ gnu/local.mk
@@ -1580,6 +1580,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/libgeotiff-fix-tests-with-proj-9.3.0.patch	\
   %D%/packages/patches/libgeotiff-fix-tests-with-proj-9.3.1.patch	\
   %D%/packages/patches/libgeotiff-fix-tests-on-i386.patch	\
+  %D%/packages/patches/libical-test-removal.patch               \
   %D%/packages/patches/libinput-udev-program-absolute-path.patch	\
   %D%/packages/patches/libobjc2-unbundle-robin-map.patch	\
   %D%/packages/patches/libvirt-add-install-prefix.patch	\
