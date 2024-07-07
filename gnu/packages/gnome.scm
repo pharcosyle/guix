2417b42af3f3f85e3195e096ab34768c274aaec0
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-07-07 13:36:53 -0700

    <workaround> network-manager gi introspection issue
    No idea why it's happening. Seems like it should be same issue I had
    with libsecret and modem-manager owing to the glib bump but it kinda
    doesn't seem so. It's definitely not the 1.44 -> 1.48.4 bump I did.
    
    ------------------------------------------------
    FAILED: src/libnm-client-impl/nm-settings-docs-gir-nmcli.xml
    /gnu/store/gs9d7swhbz2w58p57h1a8j5ny0jfag5a-meson-1.5.1/bin/meson --internal exe --unpickle /tmp/guix-build-network-manager-1.49.4.drv-0/build/meson-private/meson_exe_env_06bf74044a507da7fe69c1c77c8595fbc8905174.dat
    while executing ['/gnu/store/rd6xv2nns0iyifip393j1hwl16ni5ym1-coreutils-9.5/bin/env', 'GI_TYPELIB_PATH=/tmp/guix-build-network-manager-1.49.4.drv-0/build/src/libnm-client-impl:/gnu/store/vfk3rmqpy3cky1z80z15yln5gnh4yr90-gobject-introspection-1.80.1/lib/girepository-1.0:/gnu/store/4h1gbyra21dsjk6yjaj80w8gqskw17wa-libgudev-238/lib/girepository-1.0:/gnu/store/1skxkfkf6dij08rmqra8db4sbfbgg3n9-modem-manager-1.23.9-dev/lib/girepository-1.0:/gnu/store/wwh643n2bij5dy6i3czk1yf1r6r5cxmn-polkit-125/lib/girepository-1.0:/gnu/store/bplclcwgwlfvqa1b900f9mgcrqvbpwbs-glib-2.81.1/lib/girepository-1.0\n', 'LD_LIBRARY_PATH=/tmp/guix-build-network-manager-1.49.4.drv-0/build/src/libnm-client-impl', '/gnu/store/g32q8hdsg7g1hgysp137qpli5f3fsc2m-python-3.12.4/bin/python3', '/tmp/guix-build-network-manager-1.49.4.drv-0/NetworkManager-1.49.4/tools/generate-docs-nm-settings-docs-gir.py', '--lib-path', '/tmp/guix-build-network-manager-1.49.4.drv-0/build/src/libnm-client-impl', '--gir', 'src/libnm-client-impl/NM-1.0.gir', '--output', 'src/libnm-client-impl/nm-settings-docs-gir-nmcli.xml', '--target', 'nmcli']
    
    Change-Id: Iea948d8bfaf8e999f937362d3556bd87281e71ea
    --- stdout ---
    
    --- stderr ---
    Traceback (most recent call last):
      File "/gnu/store/if9h7wpzpwp68mz9vdl6b8yrm0rc1x6g-python-pygobject-3.48.2/lib/python3.12/site-packages/gi/importer.py", line 139, in create_module
        introspection_module = get_introspection_module(namespace)
                               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/gnu/store/if9h7wpzpwp68mz9vdl6b8yrm0rc1x6g-python-pygobject-3.48.2/lib/python3.12/site-packages/gi/module.py", line 267, in get_introspection_module
        module = IntrospectionModule(namespace, version)
                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/gnu/store/if9h7wpzpwp68mz9vdl6b8yrm0rc1x6g-python-pygobject-3.48.2/lib/python3.12/site-packages/gi/module.py", line 114, in __init__
        repository.require(namespace, version)
    gi.RepositoryError: Typelib file for namespace 'GObject', version '2.0' not found
    
    During handling of the above exception, another exception occurred:
    
    Traceback (most recent call last):
      File "/tmp/guix-build-network-manager-1.49.4.drv-0/NetworkManager-1.49.4/tools/generate-docs-nm-settings-docs-gir.py", line 14, in <module>
        from gi.repository import GIRepository
      File "/gnu/store/if9h7wpzpwp68mz9vdl6b8yrm0rc1x6g-python-pygobject-3.48.2/lib/python3.12/site-packages/gi/importer.py", line 141, in create_module
        raise ImportError(e)
    ImportError: Typelib file for namespace 'GObject', version '2.0' not found
    ------------------------------------------------
    
    Change-Id: Ia3c8bc3ccdb16679f7c7b2cf5ef22198c4420059

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -8465,7 +8465,7 @@ users.")
          "-Dnm_cloud_setup=false"
          "-Dlibaudit=yes"
          "-Dqt=false"
-         "-Ddocs=true"
+         "-Ddocs=false"
          "--sysconfdir=/etc"
          "--localstatedir=/var"
          (string-append "-Dudev_dir="
@@ -8525,7 +8525,9 @@ users.")
                                                            directory))
                           (delete-file-recursively
                            (string-append #$output directory)))
-                        '("/share/doc" "/share/gtk-doc")))))))
+                        '("/share/doc"
+                          ;; "/share/gtk-doc"
+                          )))))))
     (propagated-inputs
      (list glib))
     (native-inputs
