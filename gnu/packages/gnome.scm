ad3c8b876c0806334349fb7343e1c65cf3c64d8a
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-07-07 13:36:53 -0700

    <workaround> network-manager gi introspection issue
    No idea why it's happening. Seems like it should be same issue I had
    with libsecret and modem-manager owing to the glib bump but it kinda
    doesn't seem so. It's definitely not the 1.44 -> 1.48.4 bump I did.
    
    Change-Id: Ia3c8bc3ccdb16679f7c7b2cf5ef22198c4420059

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -8414,7 +8414,7 @@ users.")
          "-Dnm_cloud_setup=false"
          "-Dlibaudit=yes"
          "-Dqt=false"
-         "-Ddocs=true"
+         "-Ddocs=false"
          "--sysconfdir=/etc"
          "--localstatedir=/var"
          (string-append "-Dudev_dir="
@@ -8470,7 +8470,9 @@ users.")
                                                            directory))
                           (delete-file-recursively
                            (string-append #$output directory)))
-                        '("/share/doc" "/share/gtk-doc")))))))
+                        '("/share/doc"
+                          ;; "/share/gtk-doc"
+                          )))))))
     (propagated-inputs
      (list glib))
     (native-inputs
