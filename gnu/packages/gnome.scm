e65dab300d828d7f0bd655cbd959a558a365709c
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-07-07 23:41:39 -0700

    network-manager-no-logind: back to the drawing board
    Change-Id: If25e1842f866fe82237af018ba6581f8ff0dc019

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -8505,6 +8505,8 @@ services.")
        (modify-inputs (package-propagated-inputs base)
          (delete "elogind"))))))
 
+(define-public network-manager-no-logind network-manager)
+
 (define-public network-manager-openvpn
   (package
     (name "network-manager-openvpn")
