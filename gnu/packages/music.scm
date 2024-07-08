243abf3c8939efe8b38f13e8830aeb5278ef46ae
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-07-07 22:55:12 -0700

    tkinter: added back to python2, changed usages
    Change-Id: Ibca8ee3a69d401a045cf63581c6a955c65e23234

--- gnu/packages/music.scm
+++ gnu/packages/music.scm
@@ -4591,7 +4591,7 @@ standard MIDI file with the csvmidi program.")
        (list alsa-lib
              boost
              jack-2
-             `(,python "tk")
+             `(,python-tkinter "tk")
              python-dbus
              python-decorator
              python-pyinotify
