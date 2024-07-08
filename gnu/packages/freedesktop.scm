1efec7205df1c4c1636715b4d1b753fabdc32bab
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-07-07 19:17:02 -0700

    libliftoff: improve package
    Change-Id: I7c7640a47b4aeeeb65b4f42ce9f61c19e0118129

--- gnu/packages/freedesktop.scm
+++ gnu/packages/freedesktop.scm
@@ -372,7 +372,7 @@ tests.")
                 "163g8ndsbma7acy2k9mrnvlpb7yi4431hgkx1gygkafgwpq1ii1x"))))
     (build-system meson-build-system)
     (native-inputs (list pkg-config))
-    (inputs (list libdrm))
+    (propagated-inputs (list libdrm))
     (home-page "https://gitlab.freedesktop.org/emersion/libliftoff")
     (synopsis "Lightweight KMS plane library for compositors")
     (description "Libliftoff eases the use of
