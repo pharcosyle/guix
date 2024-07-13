286150b84de94fd65ba75138860439ba7c94fd51
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-07-12 18:54:26 -0700

    gtk4: remove unused ffmpeg dependency (move to other gtk4 stuff)
    From the repo NEWS file: "Drop the experimental ffmpeg backend. It
    hasn't been building for a year"
    
    Change-Id: Iaf1f07562025d2dc4781848327442f9d523015e5

--- gnu/packages/gtk.scm
+++ gnu/packages/gtk.scm
@@ -1366,7 +1366,6 @@ application suites.")
     (inputs
      (list colord                       ;for color printing support
            cups                         ;for CUPS print-backend
-           ffmpeg                       ;for ffmpeg media-backend
            fribidi
            gi-docgen
            gstreamer                    ;for gstreamer media-backend
