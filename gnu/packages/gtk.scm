b36c6eb03e0aa7fb4a945e1f08117b2820c8bf7c
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-07-22 03:37:05 -0700

    fixup "ton of stuff..." (bump gtk+ a tiny bit)
    Change-Id: Ibb0fc56623d7e499f86c9b1f29dd075d7ae00975

--- gnu/packages/gtk.scm
+++ gnu/packages/gtk.scm
@@ -1009,7 +1009,7 @@ application suites.")
   (package
     (inherit gtk+-2)
     (name "gtk+")
-    (version "3.24.42")
+    (version "3.24.43")
     (source
      (origin
        (method url-fetch)
@@ -1018,7 +1018,7 @@ application suites.")
                            name "-" version ".tar.xz"))
        (sha256
         (base32
-         "1qkp4c8xxqi58ylpizgn97qyb06kifgp2nfppc0xvm4ja1hrzy2h"))
+         "1izky8dxaxp4bg5nii4n58dgpkw79mvmvbkldf04n0qmhmjg013y"))
        (patches (search-patches "gtk3-respect-GUIX_GTK3_PATH.patch"
                                 "gtk3-respect-GUIX_GTK3_IM_MODULE_FILE.patch"))))
     (outputs '("out" "bin" "doc"))
