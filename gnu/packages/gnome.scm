bf440591478946064e562ee37b599a5d923cc035
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-08-04 23:25:01 -0700

    (TRYING OUT) Revert "<workaround> network-manager gi introspection issue"
    This reverts commit ad3c8b876c0806334349fb7343e1c65cf3c64d8a.
    
    Change-Id: I205668460b2a7a0861743de669a741a156fe423b

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -8427,7 +8427,7 @@ users.")
          "-Dnm_cloud_setup=false"
          "-Dlibaudit=yes"
          "-Dqt=false"
-         "-Ddocs=false"
+         "-Ddocs=true"
          "--sysconfdir=/etc"
          "--localstatedir=/var"
          (string-append "-Dudev_dir="
@@ -8483,9 +8483,7 @@ users.")
                                                            directory))
                           (delete-file-recursively
                            (string-append #$output directory)))
-                        '("/share/doc"
-                          ;; "/share/gtk-doc"
-                          )))))))
+                        '("/share/doc" "/share/gtk-doc")))))))
     (propagated-inputs
      (list glib))
     (native-inputs
