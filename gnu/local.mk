710c04d6d96b1c4d27fd9ba6c36cdad3948e6a70
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-08-06 13:48:41 -0700

    bump iniparser
    Change-Id: I5802bdd9bf4fd3d5aff06608dec56a559f1715c3

--- gnu/local.mk
+++ gnu/local.mk
@@ -1543,6 +1543,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/idris-test-ffi008.patch			\
   %D%/packages/patches/igraph-fix-varargs-integer-size.patch	\
   %D%/packages/patches/ilmbase-fix-tests.patch			\
+  %D%/packages/patches/iniparser-no-git-fetchcontent.patch	\
   %D%/packages/patches/instead-use-games-path.patch		\
   %D%/packages/patches/intltool-perl-compatibility.patch	\
   %D%/packages/patches/irrlicht-use-system-libs.patch		\
