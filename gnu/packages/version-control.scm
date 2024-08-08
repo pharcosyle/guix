0163d89d51afe1c81be91e73b0ce29d9385333a1
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-08-07 17:02:13 -0700

    libgit2: promote libgit2 1.8 to default
    Change-Id: I38eb705a0ff3cb082018fe7f3ae72bf203c0b87c

--- gnu/packages/version-control.scm
+++ gnu/packages/version-control.scm
@@ -1266,9 +1266,8 @@ write native speed custom Git applications in any language with bindings.")
     ;; GPLv2 with linking exception
     (license license:gpl2)))
 
-(define-public libgit2
-  ;; Default version of libgit2.
-  libgit2-1.5)
+;; Avoid having reording/renaming any vars for the moment.
+(define libgit2 libgit2-1.5)
 
 (define-public libgit2-1.7
   (package
@@ -1328,6 +1327,10 @@ write native speed custom Git applications in any language with bindings.")
                                "-DUSE_HTTP_PARSER=http-parser"
                                arg))
                          #$flags))))))
+
+(define-public libgit2
+  ;; Default version of libgit2.
+  libgit2-1.8)
 
 (define-public libgit2-1.6
   (package
