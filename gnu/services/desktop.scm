42ec0436830cfe8528c7a48bfd15cdef87f849d4
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-08-25 07:39:37 -0700

    add rtkit-service-type to default desktop-services
    Some desktop applications silently attempt to use it if
    available (notably Firefox). Seems like a good default to have.
    
    Change-Id: Ibfa6ff0b9d3a8800ad45e3bf5f5836a389e5d183

--- gnu/services/desktop.scm
+++ gnu/services/desktop.scm
@@ -2185,6 +2185,7 @@ applications needing access to be root.")
          (service geoclue-service-type)
          (service polkit-service-type)
          (service elogind-service-type)
+         (service rtkit-service-type)
          (service dbus-root-service-type)
 
          (service ntp-service-type)
