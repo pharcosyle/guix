91dd5e803ed56a4ce31a62c5ead8a77decaecd44
Sharlatan Hellseher <sharlatanus@gmail.com>
2024-11-05 13:07:27 +0000

    gnu: python-jsonpickle: Adjust inputs.
    * gnu/packages/python-web.scm (python-jsonpickle) [native-inputs]: Add
    python-wheel.
    
    Change-Id: Ib60e5786e547f551efa32a29dfe6f74b227ffb8e

--- gnu/packages/python-web.scm
+++ gnu/packages/python-web.scm
@@ -1611,6 +1611,7 @@ CSS3 that adds programming capabilities and some other syntactic sugar.")
            python-simplejson
            python-sqlalchemy
            python-ujson
+           python-wheel
            tzdata-for-tests))
     (home-page "https://jsonpickle.github.io/")
     (synopsis "Serialize object graphs into JSON")
