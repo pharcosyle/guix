e78164cba0bf345bb92a488d75075f463281aee0
Krzysztof Baranowski <pharcosyle@gmail.com>
2024-11-08 22:40:10 -0800

    Revert "<workaround> libei: disable tests"
    This reverts commit e7bf0a3a012f693f0a7cf74ebd6fbd84e326e384.

--- gnu/packages/freedesktop.scm
+++ gnu/packages/freedesktop.scm
@@ -837,7 +837,6 @@ other applications that need to directly deal with input devices.")
     (build-system meson-build-system)
     (arguments
      (list
-      #:tests? #f
       #:configure-flags #~'("-Ddocumentation=api" ;protocol requires hugo
                             "-Dsd-bus-provider=basu")))
     (inputs
