1a1e6f08569058a9053e660e9ab841a2b6929514
Sharlatan Hellseher <sharlatanus@gmail.com>
2024-11-09 08:56:59 +0000

    gnu: Remove beets-next.
    * gnu/packages/music.scm (beets-next): Delete variable.
    
    Change-Id: Id0aad5c6f2a0bc1abe8cfedc978e26b3a0f9b041

--- gnu/packages/music.scm
+++ gnu/packages/music.scm
@@ -4291,9 +4291,6 @@ Then it provides a variety of tools for manipulating and accessing
 your music.")
     (license license:expat)))
 
-(define-public beets-next
-  (deprecated-package "beets-next" beets))
-
 (define-public beets-bandcamp
   (package
     (name "beets-bandcamp")
