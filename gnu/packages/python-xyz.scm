bb488a1b3c559d21575de55cd4cdb30b6009e521
Sharlatan Hellseher <sharlatanus@gmail.com>
2024-11-22 17:06:18 +0000

    gnu: python-jupyter-core: Fix tests.
    * gnu/packages/python-xyz.scm (python-jupyter-core) [native-inputs]: Add
    python-pip.
    
    Change-Id: I6c2c553cd6a0025fca29ca8bbb51d73f957c53c1

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -12842,6 +12842,7 @@ container data structures in Python).")
                       "/etc/jupyter/migrated")))))))
     (propagated-inputs (list python-platformdirs python-traitlets))
     (native-inputs (list python-hatchling
+                         python-pip
                          python-pre-commit
                          python-pytest
                          python-pytest-cov
