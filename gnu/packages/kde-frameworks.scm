71b8188991192f28163e3ad1e9c7e7e83ee31211
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-11-26 21:33:12 +0800

    gnu: bluez-qt: Update to 6.8.0.
    * gnu/packages/kde-frameworks.scm (bluez-qt): Update to 6.8.0.
    
    Change-Id: Id57a26c68cf3dcc89b5451d9f91a265a32492616

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -388,7 +388,7 @@ http://freedesktop.org/wiki/Specifications/open-collaboration-services/")
 (define-public bluez-qt
   (package
     (name "bluez-qt")
-    (version "6.5.0")
+    (version "6.8.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -397,7 +397,7 @@ http://freedesktop.org/wiki/Specifications/open-collaboration-services/")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "0pm6dbn0za3pp078ri4rbfdb0xa163pnx26h103l3y8szik4f8zh"))))
+                "0xhdfkh3ba70xbydc2y17si9gcwpmp8wdp5jxvyqvj59qpvqj6ip"))))
     (build-system cmake-build-system)
     (native-inputs
      (list dbus extra-cmake-modules))
