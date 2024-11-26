5f9404c43b5b84972ee5a18406ca903ab9b58dca
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-11-26 21:41:14 +0800

    gnu: kdbusaddons: Update to 6.8.0.
    * gnu/packages/kde-frameworks.scm (kdbusaddons): Update to 6.8.0.
    
    Change-Id: I530e6a2c1ed92d147d88c196f61ea913be935463

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -886,7 +886,7 @@ many more.")
 (define-public kdbusaddons
   (package
     (name "kdbusaddons")
-    (version "6.5.0")
+    (version "6.8.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -895,7 +895,7 @@ many more.")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "1zm8y0ganicalqq1s2apm5ikzpza6f6m0607aaad4bbsbpzzkb5g"))))
+                "1iwbsp4cwiqxw9yzhdqfdk3m06dxz9dmfravh8phmbvjqc6hlxkc"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules dbus qttools))
