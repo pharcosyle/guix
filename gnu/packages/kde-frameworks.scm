8ac480b9cd8756fdaaaef46382c3289bade79ce0
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-11-27 20:02:38 +0800

    gnu: kwayland: Update to 6.2.4.
    * gnu/packages/kde-frameworks.scm (kwayland): Update to 6.2.4.
    
    Change-Id: Ib5789360457c6e169c9ffb1154a764a3ed2843d3

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -1550,7 +1550,7 @@ protocols used in KDE Plasma.")
 (define-public kwayland
   (package
     (name "kwayland")
-    (version "6.1.4")
+    (version "6.2.4")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/plasma/"
@@ -1558,7 +1558,7 @@ protocols used in KDE Plasma.")
                                   version ".tar.xz"))
               (sha256
                (base32
-                "0x4psmfn993lvha8cjw6w55gp7jp3490sb44wjfp6b5ccmpc14c6"))))
+                "1b1lrpq2d21pgdd35na5yl67gpdz2cpph6c00m00r6wfiybnkzpl"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules pkg-config
