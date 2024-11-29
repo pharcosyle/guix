11ac81afaead6fe68928b75d62e89cabcb558195
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-11-29 22:21:03 +0800

    gnu: akonadi-contacts: Update to 24.08.3.
    * gnu/packages/kde-pim.scm (akonadi-contacts): Update to 24.08.3.
    
    Change-Id: Ie497047aafea53b9736916090357c1dd1fa51cee

--- gnu/packages/kde-pim.scm
+++ gnu/packages/kde-pim.scm
@@ -187,14 +187,14 @@ collection and item views.")
 (define-public akonadi-contacts
   (package
     (name "akonadi-contacts")
-    (version "24.05.2")
+    (version "24.08.3")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/akonadi-contacts-" version ".tar.xz"))
        (sha256
-        (base32 "1207dgilr5y4b3g3fk2ywyvb6mryq2xrpkhi6cyhgn8k84q201fn"))))
+        (base32 "1jii0lph4fbx23rd0qbhp78qz1wgn2crydhgsb9ghp7qmqfsp0ks"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules))
