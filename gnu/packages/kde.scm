0f3e788e3b7819b1c5329fa9053e0bf0ebcb3ccb
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-11-29 21:13:54 +0800

    gnu: libkomparediff2: Update to 24.08.3.
    * gnu/packages/kde.scm (libkomparediff2): Update to 24.08.3.
    [inputs]: Remove kcodecs-5, kconfig-5, kcoreaddons-5, ki18n-5, kio-5,
    kxmlgui-5, and qtbase-5; add kcodecs, kconfig, kcoreaddons, ki18n,
    kio, kxmlgui, and qtbase.
    
    Change-Id: Ic9230bbed64c8f6825993d73bdaf5aa3a2e8eccd

--- gnu/packages/kde.scm
+++ gnu/packages/kde.scm
@@ -1035,24 +1035,24 @@ accessibility clients such as screen readers.")
 (define-public libkomparediff2
   (package
     (name "libkomparediff2")
-    (version "24.05.2")
+    (version "24.08.3")
     (source
-      (origin
-        (method url-fetch)
-        (uri (string-append "mirror://kde/stable/release-service/" version
-                            "/src/libkomparediff2-" version ".tar.xz"))
-        (sha256
-         (base32 "1g4zjsdd49n2kh5m8ijm9cm95wfn2rglgnmvvr0ap1iidy4843hy"))))
+     (origin
+       (method url-fetch)
+       (uri (string-append "mirror://kde/stable/release-service/" version
+                           "/src/libkomparediff2-" version ".tar.xz"))
+       (sha256
+        (base32 "0rvsx5ljc5g7vwkmparyfbqz6jh3dn7w4548wbl1wvnmfzn9i47d"))))
     (native-inputs
      (list extra-cmake-modules pkg-config))
     (inputs
-     (list kcodecs-5
-           kconfig-5
-           kcoreaddons-5
-           ki18n-5
-           kio-5
-           kxmlgui-5
-           qtbase-5))
+     (list kcodecs
+           kconfig
+           kcoreaddons
+           ki18n
+           kio
+           kxmlgui
+           qtbase))
     (build-system cmake-build-system)
     (home-page "https://kde.org")
     (synopsis "Library to compare files and strings, used in Kompare and KDevelop")
