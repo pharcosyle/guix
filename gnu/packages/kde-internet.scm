90845139187d62ebd95c628eaff16635fb3f540f
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-12-02 19:56:49 +0800

    gnu: ktorrent: Update to 24.08.3.
    * gnu/packages/kde-internet.scm (ktorrent): Update to 24.08.3.
    
    Change-Id: Iae0c1ab3fbb30859c015945af39a45e6a5497678

--- gnu/packages/kde-internet.scm
+++ gnu/packages/kde-internet.scm
@@ -431,14 +431,14 @@ This package is part of the KDE networking module.")
 (define-public ktorrent
   (package
     (name "ktorrent")
-    (version "24.05.2")
+    (version "24.08.3")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/ktorrent-" version ".tar.xz"))
        (sha256
-        (base32 "1m96ch4rfrjnrw9rd7ad3ipc0hvhxl2awb7aqfnk4dd1g8hhjkq0"))))
+        (base32 "0h5dgk8sashlq44jgqk1f1pjw0yvlskvj1nhl2l1v9axpnyvj19p"))))
     (build-system qt-build-system)
     (arguments (list #:qtbase qtbase))
     (native-inputs
