abbb763b8832d61e5c2ec3054409680bb23c0349
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-12-02 19:54:01 +0800

    gnu: kget: Update to 24.08.3.
    * gnu/packages/kde-internet.scm (kget): Update to 24.08.3.
    
    Change-Id: Ifbe1cca7545cefeeb54c46347a961961eed39050

--- gnu/packages/kde-internet.scm
+++ gnu/packages/kde-internet.scm
@@ -168,14 +168,14 @@ Other notable features include:
 (define-public kget
   (package
     (name "kget")
-    (version "24.05.2")
+    (version "24.08.3")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/kget-" version ".tar.xz"))
        (sha256
-        (base32 "0xm3a3bxk4gb1yxpq3icg1wh5sqpmxqlr9n8j1gffszzd6c9x8mn"))))
+        (base32 "106qfxk6irzibd5mvvb92m71395b8zkm4l34c5yblfgdfs6l507c"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules kdoctools pkg-config))
